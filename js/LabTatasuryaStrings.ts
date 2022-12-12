8fd48709b33a2fef067bd3823ba0dfea6351e850
Mario <ardamomo3@gmail.com>
2022-12-12 09:07:17 +0700

    grunt update: add images

--- js/LabTatasuryaStrings.ts
+++ js/LabTatasuryaStrings.ts
@@ -16,7 +16,67 @@ type StringsType = {
   'screen': {
     'name': string;
     'nameStringProperty': LinkableProperty<string>;
-  }
+  };
+  'ourSun': string;
+  'ourSunStringProperty': LinkableProperty<string>;
+  'earth': string;
+  'earthStringProperty': LinkableProperty<string>;
+  'earthDays': string;
+  'earthDaysStringProperty': LinkableProperty<string>;
+  'pattern': {
+    '0value': {
+      '1units': string;
+      '1unitsStringProperty': LinkableProperty<string>;
+    }
+  };
+  'planet': string;
+  'planetStringProperty': LinkableProperty<string>;
+  'planetMass': string;
+  'planetMassStringProperty': LinkableProperty<string>;
+  'satellite': string;
+  'satelliteStringProperty': LinkableProperty<string>;
+  'satelliteMass': string;
+  'satelliteMassStringProperty': LinkableProperty<string>;
+  'star': string;
+  'starStringProperty': LinkableProperty<string>;
+  'starMass': string;
+  'starMassStringProperty': LinkableProperty<string>;
+  'moon': string;
+  'moonStringProperty': LinkableProperty<string>;
+  'moonMass': string;
+  'moonMassStringProperty': LinkableProperty<string>;
+  'earthMasses': string;
+  'earthMassesStringProperty': LinkableProperty<string>;
+  'earthMass': string;
+  'earthMassStringProperty': LinkableProperty<string>;
+  'clear': string;
+  'clearStringProperty': LinkableProperty<string>;
+  'kilometers': string;
+  'kilometersStringProperty': LinkableProperty<string>;
+  'thousandEarthMasses': string;
+  'thousandEarthMassesStringProperty': LinkableProperty<string>;
+  'v': string;
+  'vStringProperty': LinkableProperty<string>;
+  'returnObjects': string;
+  'returnObjectsStringProperty': LinkableProperty<string>;
+  'gravity': string;
+  'gravityStringProperty': LinkableProperty<string>;
+  'gravityForce': string;
+  'gravityForceStringProperty': LinkableProperty<string>;
+  'off': string;
+  'offStringProperty': LinkableProperty<string>;
+  'on': string;
+  'onStringProperty': LinkableProperty<string>;
+  'velocity': string;
+  'velocityStringProperty': LinkableProperty<string>;
+  'mass': string;
+  'massStringProperty': LinkableProperty<string>;
+  'path': string;
+  'pathStringProperty': LinkableProperty<string>;
+  'grid': string;
+  'gridStringProperty': LinkableProperty<string>;
+  'measuringTape': string;
+  'measuringTapeStringProperty': LinkableProperty<string>;
 };
 
 const LabTatasuryaStrings = getStringModule( 'LAB_TATASURYA' ) as StringsType;
