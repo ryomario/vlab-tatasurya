012d990a6d3d9b1d9eece0766d3d6ed15b20d09e
Mario <ardamomo3@gmail.com>
2022-12-14 09:51:37 +0700

    style: Panel remove unused settings
    Menghilangkan pengaturan yang tidak diperlukan.

--- js/common/view/LabTatasuryaControls.ts
+++ js/common/view/LabTatasuryaControls.ts
@@ -29,13 +29,29 @@ public constructor( model: LabTatasuryaModel, screenView: Interruptable, provide
 
     const options: LabTatasuryaControlsOptions = merge( {}, LabTatasuryaConstants.CONTROL_PANEL_OPTIONS, providedOptions ) as unknown as LabTatasuryaControlsOptions;
 
+    const children = [];
+
+    if ( model.getScenes().length > 1 ) {
+        children.push(
+            new SceneSelectionControls( model.sceneProperty, model.getScenes(), screenView, merge( { tandem: options.tandem.createTandem( 'sceneControl' ) }, MENU_SECTION_OPTIONS ) )
+        );
+        children.push(
+            new HSeparator()
+        );
+    }
+
+    children.push(
+        new CheckboxPanel( model, merge( { tandem: options.tandem.createTandem( 'checkboxPanel' ) }, MENU_SECTION_OPTIONS ) )
+    );
+
     super( {
         children: [
-            new SceneSelectionControls( model.sceneProperty, model.getScenes(), screenView, merge( { tandem: options.tandem.createTandem( 'sceneControl' ) }, MENU_SECTION_OPTIONS ) ),
-            new HSeparator(),
-            new GravityControl( model.gravityEnabledProperty, merge( { tandem: options.tandem.createTandem( 'gravityControl' ) }, MENU_SECTION_OPTIONS ) ),
-            new HSeparator(),
-            new CheckboxPanel( model, merge( { tandem: options.tandem.createTandem( 'checkboxPanel' ) }, MENU_SECTION_OPTIONS ) )
+            // new SceneSelectionControls( model.sceneProperty, model.getScenes(), screenView, merge( { tandem: options.tandem.createTandem( 'sceneControl' ) }, MENU_SECTION_OPTIONS ) ),
+            // new HSeparator(),
+            // new GravityControl( model.gravityEnabledProperty, merge( { tandem: options.tandem.createTandem( 'gravityControl' ) }, MENU_SECTION_OPTIONS ) ),
+            // new HSeparator(),
+            // new CheckboxPanel( model, merge( { tandem: options.tandem.createTandem( 'checkboxPanel' ) }, MENU_SECTION_OPTIONS ) )
+            ...children
         ],
         spacing: 4,
         y: 5,
